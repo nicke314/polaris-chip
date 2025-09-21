7c73a4d901f8a6cd57373df2fa19e627c1ad9614
Nicholas Edwards <nbe5073@psu.edu>
2025-09-21 17:17:31 -0400

    Added cards in

--- src/my-card.js
+++ src/my-card.js
@@ -14,7 +14,10 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "My Card";
+    this.bgc = 'blue';
+    this.name = "Beaver Stadium";
+    this.brdc = 'black';
   }
 
   static get styles() {
@@ -22,16 +25,62 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+      .card {
+      background-color: darkblue;
+      padding: 10px;
+      margin: 6px;
+      max-width: 400px;
+      text-align: center;
+      border: 8px solid;
+      border-color: black;
+      color: lightgray;
+    }
+    .hd1 {
+      color: white;
+    }
+    .img1 {
+      max-height: 100%;
+      max-width: 100%;
+    }
+    .btn {
+      display: none;
+    
+    }
+    .btn:hover,
+    .btn:focus {
+      color:white;
+      background-color:blue;
+    }
+    #wrapper {
+    display: flex;
+    }
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`<div>${this.title}</div>
+    <div id="wrapper">
+      <div class="card original"
+        style="background-color: ${this.bgc}; border-color: ${this.brdc};">
+        <h1 class="hd1">${this.name}</h1>
+          <img class="img1"   src="https://gopsusports.com/imgproxy/xuPVPshSzLbxQ2PZazgWiKESdVoGiQg7eD4KJZhMK60/rs:fit:1980:0:0/g:ce/q:90/aHR0cHM6Ly9zdG9yYWdlLmdvb2dsZWFwaXMuY29tL2dvcHN1c3BvcnRzLXByb2QvMjAyNC8wNS8xMy9Oa3NGRENvOURDQWdTRlgyTVFqTlRROTN5ejBKb1J3TWZPcWhXWjluLmpwZw.jpg" alt="Image of Beaver Stadium" max-width: "75%">
+  
+       <p class="bsp">This is an image of beaver stadium at night before a white out game as the Penn State football team is running out onto the field. There are fireworks in the sky, and everyone is wearing white. Technically the stadium is West Shore Home Field at Beaver Stadium, but nobody calls it that. </p> 
+  
+        <a href="https://hax.psu.edu">
+        <button class="btn">Details</button>
+        </a>
+      </div>
+    </div>
+    `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      bgc: { type: String },
+      name: { type: String },
+      brdc: { type: String},
     };
   }
 }
